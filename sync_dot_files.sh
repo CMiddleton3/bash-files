5d2765d9a7e3c809cd698241dea63c0d5baa6a7f
ClarenceCM3 <ClarenceCM3@gmail.com>
2025-01-12 12:41:07 -0600

    Using $HOME instead of ~

--- sync_dot_files.sh
+++ sync_dot_files.sh
@@ -2,7 +2,7 @@
 
 # Define the directory path for the Git repository
 echo "Defining repository directory: $repo_dir"
-repo_dir="~/bash-files"
+repo_dir="$HOME/bash-files"
 
 # Check if the Git repository directory exists
 if [ ! -d "$repo_dir" ]; then
@@ -23,36 +23,36 @@ git pull
 if [ ! -z "$(git diff --name-only)" ]; then
     # Copy the dot files to your home directory only if there are changes
     echo "Copying .bashrc"
-    cp .bashrc ~/.bashrc
+    cp .bashrc $HOME/.bashrc
     echo "Copying .bash_profile"
-    cp .bash_profile ~/.bash_profile
+    cp .bash_profile $HOME/.bash_profile
     echo "Copying .bash_aliases"
-    cp .bash_aliases ~/.bash_aliases
+    cp .bash_aliases $HOME/.bash_aliases
     echo "Copying .win_aliases"
-    cp .win_aliases ~/.win_aliases
+    cp .win_aliases $HOME/.win_aliases
     echo "Copying .misc_aliases"
-    cp .misc_aliases ~/.misc_aliases
+    cp .misc_aliases $HOME/.misc_aliases
     echo "Copying .vimrc"
-    cp .vimrc ~/.vimrc
+    cp .vimrc $HOME/.vimrc
     echo "Copying .tmux.conf"
-    cp .tmux.conf ~/.tmux.conf
+    cp .tmux.conf $HOME/.tmux.conf
     # Source the updated dot files only if there are changes
     echo "Sourcing updated dot files"
-    source ~/.bash_profile
+    source $HOME/.bash_profile
 else
     # If there are no changes, echo a message indicating this
     echo "No changes in repository."
 fi
 
 # Create a symlink to keep the .dot file synced only if it doesn't already exist
-if [ ! -e ~/sync_dot_files.sh ]; then
+if [ ! -e $HOME/sync_dot_files.sh ]; then
     echo "Creating symlink for sync_dot_files.sh"
-    chmod +x ~/bash-files/sync_dot_files.sh
-    ln -s ~/bash-files/sync_dot_files.sh ~/sync_dot_files.sh
+    chmod +x $HOME/bash-files/sync_dot_files.sh
+    ln -s $HOME/bash-files/sync_dot_files.sh $HOME/sync_dot_files.sh
 fi
 
 # Source the updated dot files
 if [ ! -z "$(git diff --name-only)" ]; then
     echo "Sourcing updated dot files"
-    source ~/.bash_profile
+    source $HOME/.bash_profile
 fi
